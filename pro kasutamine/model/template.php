503685e1e1dfec0e51a43b55aed5202430e6e049
kermoliblik <kermo.liblik@khk.ee>
2018-01-18 10:29:54 +0200

    Template object properties

---
+++ pro kasutamine/model/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dante
+ * Date: 18-Jan-18
+ * Time: 10:22
+ */
+
+class template
+{
+    //template object properties
+    var $file = ''; //html view file name
+    var $content = false; //content for HTML file
+    var $vars = array(); // values for HTML view template
+
+
+}
