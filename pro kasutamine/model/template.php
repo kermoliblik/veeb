3dfae3752ad7e5ded910d4a65320b9948498b430
kermoliblik <kermo.liblik@khk.ee>
2018-01-18 11:02:26 +0200

    file reader, file loader and check

--- pro kasutamine/model/template.php
+++ pro kasutamine/model/template.php
@@ -23,4 +23,36 @@ class template
         $this->content = file_get_contents($f);
     }
 
+    // HTML view file check
+    function loadFile(){
+        // checking for HTML views excistence
+        if (!is_dir(VIEWS_DIR)){
+            echo 'There is no directory called '.VIEWS_DIR.' please check directory!<br />';
+            exit;
+        }
+        // views/test.html
+        $f = $this->file; // Replaces $this->file with $f
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        // test.html
+        $f = VIEWS_DIR.$this->file;
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        // if file is given as test
+        $f =VIEWS_DIR.$this->file.'.html';
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        //if file is given as directory.test
+        $f = VIEWS_DIR.str_replace('.','/',$this->file).'.html';
+        if (file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        if ($this->content === false){
+            echo 'Unable to read content from file '.$this->file.'<br />';
+        }
+    }
+
 }
