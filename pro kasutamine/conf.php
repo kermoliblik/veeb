0925475746e951683a1f997576380e2d897b88aa
kermoliblik <kermo.liblik@khk.ee>
2018-01-18 09:43:27 +0200

    Config file

---
+++ pro kasutamine/conf.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dante
+ * Date: 18-Jan-18
+ * Time: 09:35
+ */
+//config
+
+//loome vajalikud abikonstantid
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
