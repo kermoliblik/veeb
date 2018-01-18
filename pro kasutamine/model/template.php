04a8628facce4c0ffee43e745aae4761d5db4c65
kermoliblik <kermo.liblik@khk.ee>
2018-01-18 10:40:18 +0200

    file content reading

--- pro kasutamine/model/template.php
+++ pro kasutamine/model/template.php
@@ -8,10 +8,19 @@
 
 class template
 {
-    //template object properties
-    var $file = ''; //html view file name
+    // template object properties
+    var $file = ''; // html view file name
     var $content = false; //content for HTML file
     var $vars = array(); // values for HTML view template
 
+    // class tmplate methods
+    //  Reading contents from HTML view file
+
+    function readFile(){
+        /*$fp = fopen($f,'rb');
+        $this->content = fread($fp, filesize($f));
+        fclose($fp);*/
+        $this->content = file_get_contents($f);
+    }
 
 }
