62543b2530a485d41ad4aa362e66fcc1a8737951
kermoliblik <kermo.liblik@khk.ee>
2018-01-31 10:49:26 +0200

    esime ja teine leht

---
+++ pro kasutamine/controllers/esimene.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dante
+ * Date: 31-Jan-18
+ * Time: 10:26
+ */
+$mainTmpl->set('content', '1. Lehe sisu');
---
+++ pro kasutamine/controllers/teine.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dante
+ * Date: 31-Jan-18
+ * Time: 10:48
+ */
+<?php
+$mainTmpl->set('content', '2. Lehe sisu');
