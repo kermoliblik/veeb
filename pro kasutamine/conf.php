ca32c0547aa75d0595d21bd22730baa966821e94
kermoliblik <kermo.liblik@khk.ee>
2018-02-02 09:26:36 +0200

    lisatud db_conf fail configi

--- pro kasutamine/conf.php
+++ pro kasutamine/conf.php
@@ -21,5 +21,8 @@ require_once MODEL_DIR.'template.php'; // html vaade failide töötlus
 require_once MODEL_DIR.'http.php'; // HTTP töötlus klass
 require_once MODEL_DIR.'linkobject.php';//lingi tootlus klass
 require_once MODEL_DIR.'mysql.php'; //database class
+//n6uame confi
+require_once "db_conf.php";
 // loome vajalikud objektid, mis on pidevalt tööl
 $http = new linkobject();
+$db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
