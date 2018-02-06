554bfd180c93015649b7930f1fa1e27cf9d96ff9
kermoliblik <kermo.liblik@khk.ee>
2018-02-06 13:19:41 +0200

    ANDMEBAASIS VÕTAB SISU

--- pro kasutamine/controllers/default.php
+++ pro kasutamine/controllers/default.php
@@ -5,4 +5,21 @@
  * Date: 31-Jan-18
  * Time: 11:11
  */
-$mainTmpl->set('content', 'Vaikimisi sisu');
+$page_id = (int)$http->get('page_id');
+$sql = 'SELECT * FROM content '.'WHERE content_id='.fixDB($page_id);
+$result = $db->getData($sql);
+
+if ($result == false){
+    $sql = 'SELECT content FROM content'.' WHERE is_first_page='.fixDB(1);
+    $result = $db->getData($sql);
+}
+
+
+
+if ($result != false){
+    $page = $result[0];
+    $http->set('page_id',$page['content_id']);// setib page_id content idga
+    $mainTmpl->set('content', $page['content']);// sisu v'ljastus
+}
+
+
